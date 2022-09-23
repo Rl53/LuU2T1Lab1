e27c3d67623c5fea7e74fd78badb88836b0bb084
rl53 <rosannal4@nycstudents.net>
2022-09-23 11:02:28 -0400

    l

---
+++ src/CatRunner.java
@@ -0,0 +1,13 @@
+public class CatRunner {
+    public static void main(String[] args) {
+
+        // creating one instance/object from the Rectangle class "blueprint"
+        Cat cat1 = new Cat("Sparky", 5, 6.3);
+        cat1.introduce();  // calling a method on the object
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Junior", 3, 5.4);
+        cat2.introduce();  // calling a method on the object
+        cat2.printCatInfo();
+    }
+}
